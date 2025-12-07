fe6f5d8c9e2c74ce386557f3378c63d1f3649f0a
Maciej-Dybal <maciej.dybal@intive.com>
2025-12-07 11:25:39 +0100

    feat(page): dawdawaw
    dawdawda
    
    BREAKING CHANGE: awdawdawdaw

--- stories/02. Components/Page.tsx
+++ stories/02. Components/Page.tsx
@@ -44,8 +44,8 @@ export const Page: React.FC = () => {
 						such data from the "args" of child component stories
 					</li>
 					<li>
-						Assemble data in the page component from your services. You can mock
-						these services out using Storybook.
+						dkawdawdawdaowdk Assemble data in the page component from your
+						services. You can mock these services out using Storybook.
 					</li>
 				</ul>
 				<p>
