aa03bbc5b8deff5a7b310019a9661c97cbc623fc
Maciej-Dybal <maciej.dybal@intive.com>
2026-01-15 10:21:52 +0100

    feat(router): a new Router component
    * feat(test) test component addon (#14) (#15)
    
    * feat(router): a new Router component

---
+++ stories/02. Components/Router.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface RouterProps {
+	children?: React.ReactNode;
+}
+
+const Router: React.FC<RouterProps> = ({ children }) => {
+	return (
+		<div>
+			{children}
+		</div>
+	);
+};
+
+export default Router;
