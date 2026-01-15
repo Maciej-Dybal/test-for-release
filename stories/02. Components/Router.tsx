201f9214d0a66c0fbe22057dc6fdd2754d8279c5
Maciej-Dybal <maciej.dybal@intive.com>
2026-01-15 10:25:59 +0100

    fix(router): lint fix

--- stories/02. Components/Router.tsx
+++ stories/02. Components/Router.tsx
@@ -1,15 +1,11 @@
-import React from 'react';
+import type React from "react";
 
 interface RouterProps {
 	children?: React.ReactNode;
 }
 
 const Router: React.FC<RouterProps> = ({ children }) => {
-	return (
-		<div>
-			{children}
-		</div>
-	);
+	return <div>{children}</div>;
 };
 
 export default Router;
